d5bcffe9882fe729c250d508c9834706cb81aae9
Paulo Henrique Lerbach Rodrigues <palerique@gmail.com>
2012-08-02 00:16:32 -0300

    Commit inicial do projeto DOO

---
+++ doo/src/main/java/br/org/universa/doo/App.java
@@ -0,0 +1,13 @@
+package br.org.universa.doo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
