bf2422eee98d4e2b024ea18775e79044257ba19c
Bruno Oliveira <brunobdantas@gmail.com>
2012-08-14 21:49:33 -0300

    Venda - parcial

--- doo/src/main/java/br/org/universa/doo/venda/Venda.java
+++ doo/src/main/java/br/org/universa/doo/venda/Venda.java
@@ -1,5 +1,13 @@
 package br.org.universa.doo.venda;
 
+import java.util.Date;
+import java.util.List;
+
 public class Venda {
+	private Date dataDaVenda;
+	private List itensDaVenda;
+	private List pagamentosDaVenda;
+	
+	
 
 }
