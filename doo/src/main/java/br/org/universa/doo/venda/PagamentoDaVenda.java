94e76457b0c4e76b5355dfd7df6f69546f4c3419
Paulo Henrique Lerbach Rodrigues <palerique@gmail.com>
2012-08-14 21:34:38 -0300

    PagamentoDaVenda.java atualizado
    Signed-off-by: Paulo Henrique Lerbach Rodrigues <palerique@gmail.com>

--- doo/src/main/java/br/org/universa/doo/venda/PagamentoDaVenda.java
+++ doo/src/main/java/br/org/universa/doo/venda/PagamentoDaVenda.java
@@ -2,4 +2,20 @@ package br.org.universa.doo.venda;
 
 public class PagamentoDaVenda {
 
+	private int numeroDaParcela;
+	private double valorDaParcela;
+
+	public PagamentoDaVenda(int numeroDaParcela, double valorDaParcela) {
+		this.numeroDaParcela = numeroDaParcela;
+		this.valorDaParcela = valorDaParcela;
+	}
+
+	public int getNumeroDaParcela() {
+		return numeroDaParcela;
+	}
+
+	public double getValorDaParcela() {
+		return valorDaParcela;
+	}
+
 }
