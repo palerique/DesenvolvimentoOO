9b2eb986ed4d45a3c7ae55fb24618a35974388a5
Paulo Henrique Lerbach Rodrigues <palerique@gmail.com>
2012-08-14 21:46:50 -0300

    Desenvolvimento de ItemDaVenda.java
    Signed-off-by: Paulo Henrique Lerbach Rodrigues <palerique@gmail.com>

--- doo/src/main/java/br/org/universa/doo/venda/ItemDaVenda.java
+++ doo/src/main/java/br/org/universa/doo/venda/ItemDaVenda.java
@@ -2,4 +2,20 @@ package br.org.universa.doo.venda;
 
 public class ItemDaVenda {
 
+	private int quantidade;
+	private Produto produto;
+
+	public ItemDaVenda(Produto produto, int quantidade) {
+		this.produto = produto;
+		this.quantidade = quantidade;
+	}
+
+	public Produto getProduto() {
+		return produto;
+	}
+
+	public int getQuantidade() {
+		return quantidade;
+	}
+
 }
