3d86093828a16b2e209be03a7e9b50423365f2fd
Bruno Oliveira <brunobdantas@gmail.com>
2012-08-14 21:41:42 -0300

    classe Produto implementada

--- doo/src/main/java/br/org/universa/doo/venda/Produto.java
+++ doo/src/main/java/br/org/universa/doo/venda/Produto.java
@@ -1,5 +1,29 @@
 package br.org.universa.doo.venda;
 
 public class Produto {
+	
+	private int codigo;
+	private String descricao;
+	private double preco;
+	
+	public Produto(int codigo, String descricao, double preco) {
+		this.codigo = codigo;
+		this.descricao = descricao;
+		this.preco = preco;
+	
+		}
+
+	public int getCodigo() {
+		return codigo;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public double getPreco() {
+		return preco;
+	}
+	
 
 }
