d753c41f9997df9a543f084bb8426ff46383b560
Paulo Henrique Lerbach Rodrigues <palerique@gmail.com>
2012-09-02 12:42:45 -0300

    Alteração do TesteGit.java em outro branche para verificar histórico.
    Signed-off-by: Paulo Henrique Lerbach Rodrigues <palerique@gmail.com>

--- doo/src/main/java/br/com/palerique/testes/TesteGit.java
+++ doo/src/main/java/br/com/palerique/testes/TesteGit.java
@@ -1,9 +1,11 @@
 package br.com.palerique.testes;
 
 public class TesteGit {
-  
 	public static void main(String[] args) {
 		
 	}
-	
+	public TesteGit() {
+		// TODO Auto-generated constructor stub
+	}
+
 }
